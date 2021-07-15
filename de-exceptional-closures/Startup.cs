aae7f0cb5dac8c7302e1c77425b686a1067c4267
Michael Stevenson <michael.stevenson@finance-ni.gov.uk>
2021-07-15 13:25:47 +0100

    added dev error page

--- de-exceptional-closures/Startup.cs
+++ de-exceptional-closures/Startup.cs
@@ -50,7 +50,8 @@ namespace de_exceptional_closures
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseDeveloperExceptionPage();
+              //  app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
