c8f203430fe1eb06089c2945813591dc6362ffef
michael.stevenson <michael.stevenson@finance-ni.gov.uk>
2021-07-20 16:38:25 +0100

    Added datatables

---
+++ de-exceptional-closures/wwwroot/Scripts/myclosures/myclosures.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    $('#MyClosuresTable').DataTable(
+        {
+            "ordering": true,
+            "columnDefs": [{
+                "targets": 5,
+                "orderable": false,
+            }],
+            aaSorting: [],
+            bInfo: true,
+            paging: true,
+            pageLength: 10,
+            bLengthChange: true,
+            bPaginate: true,
+            bFilter: true,
+            bSortable: true,
+            order: [[2, "desc"]],
+            language: { search: "", searchPlaceholder: "Search" },
+            drawCallback: function () {
+
+            }
+        }
+    );
+
+    $('#MyClosuresTable input').addClass('form-control');
+    $('#MyClosuresTable_wrapper input').unwrap();
+    $('#MyClosuresTable_wrapper input').attr("aria-label", "search");
+    $('#MyClosuresTable_filter').css("padding-bottom", "5px");
+    $('#MyClosuresTable_filter label').addClass('hide-text');
+    $('.disabled').attr('aria-disabled', true);
+    $('.dataTables_filter input').attr('type', 'text');
+});
