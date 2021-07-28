7e2ed9a4c069c1e81b3bd27775309bbc5cb9e55e
michael.stevenson <michael.stevenson@finance-ni.gov.uk>
2021-07-28 11:15:13 +0100

    Fixed issue with dropdown and other reason

--- de-exceptional-closures/wwwroot/Scripts/Edit.js
+++ de-exceptional-closures/wwwroot/Scripts/Edit.js
@@ -1,6 +1,16 @@
-﻿
+﻿$(document).ready(function ()
+{
+    let getReasonTypeId = $("#editForm input[type='radio']:checked").val();
 
-
+    if (getReasonTypeId === "5")
+    {
+        $("#OtherReasons").show();
+    }
+    else
+    {
+        $("#OtherReasons").hide();
+    }
+});
 
 function showHideOtherReason(id)
 {
