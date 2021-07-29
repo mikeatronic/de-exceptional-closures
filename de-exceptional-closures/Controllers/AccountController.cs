4c10d2f7a0980eedf1e193776273348d0ae86fa3
michael.stevenson <michael.stevenson@finance-ni.gov.uk>
2021-07-29 17:55:53 +0100

    sq fixes

--- de-exceptional-closures/Controllers/AccountController.cs
+++ de-exceptional-closures/Controllers/AccountController.cs
@@ -19,32 +19,23 @@ namespace de_exceptional_closures.Controllers
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly INotifyService _notifyService;
+        private readonly SignInManager<IdentityUser> _signInManager;
         public readonly string TitleTagName;
 
-        public AccountController(UserManager<IdentityUser> userManager, INotifyService notifyService)
+        public AccountController(UserManager<IdentityUser> userManager, INotifyService notifyService, SignInManager<IdentityUser> signInManager)
         {
             _userManager = userManager;
             _notifyService = notifyService;
+            _signInManager = signInManager;
             TitleTagName = "Forgot your password?";
         }
 
         [HttpGet]
         [AllowAnonymous]
-        public IActionResult ForgotPasswordAsync()
+        public IActionResult ForgotPassword()
         {
             ForgotPassWordViewModel model = new ForgotPassWordViewModel();
             model.TitleTagName = "Forgot your password?";
-
-            return View(model);
-        }
-
-        [HttpGet]
-        [AllowAnonymous]
-        [RateLimiting(Name = "ForgotPasswordConfirmation", Seconds = 2)]
-        public IActionResult ForgotPasswordConfirmation()
-        {
-            BaseViewModel model = new BaseViewModel();
-            model.TitleTagName = "Forgot password confirmation";
 
             return View(model);
         }
@@ -84,14 +75,76 @@ namespace de_exceptional_closures.Controllers
 
         [HttpGet]
         [AllowAnonymous]
+        [RateLimiting(Name = "ForgotPasswordConfirmation", Seconds = 5)]
+        public IActionResult ForgotPasswordConfirmation()
+        {
+            BaseViewModel model = new BaseViewModel();
+            model.TitleTagName = "Forgot password confirmation";
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
         [RateLimiting(Name = "Register", Seconds = 5)]
         public IActionResult Register()
         {
-            BaseViewModel model = new BaseViewModel();
+            RegisterViewModel model = new RegisterViewModel();
             model.TitleTagName = "Register";
 
             return View(model);
         }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        [RateLimiting(Name = "Register", Seconds = 5)]
+        public async Task<IActionResult> RegisterAsync(RegisterViewModel model, string returnUrl = null)
+        {
+            model.TitleTagName = "Register";
+
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                   // _logger.LogInformation("User created a new account with password.");
+
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                        protocol: Request.Scheme);
+
+                    _notifyService.SendEmail(model.Email, "Confirm your email for DE exceptional closures", $"Please confirm your account by clicking this link: '{HtmlEncoder.Default.Encode(callbackUrl)}'");
+
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return RedirectToPage("RegisterConfirmation", new { email = model.Email, returnUrl = returnUrl });
+                    }
+                    else
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        return LocalRedirect(returnUrl);
+                    }
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("Password", error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
 
         [HttpGet]
         [AllowAnonymous]
