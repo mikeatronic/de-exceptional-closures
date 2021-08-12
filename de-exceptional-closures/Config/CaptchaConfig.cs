6531475385940eb58996c0a06def0d580ba347a3
michael.stevenson <michael.stevenson@finance-ni.gov.uk>
2021-08-12 08:54:52 +0100

    Removed captcha

--- de-exceptional-closures/Config/CaptchaConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace de_exceptional_closures.Config
-{
-    public class CaptchaConfig
-    {
-        public string googleUrl { get; set; }
-        public string SiteKey { get; set; }
-        public string Secret { get; set; }
-    }
-}
