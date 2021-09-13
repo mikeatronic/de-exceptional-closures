bc15c15050db2320270ec5b360527abb055a49bf
michael.stevenson <michael.stevenson@finance-ni.gov.uk>
2021-09-13 08:21:02 +0100

    Updated Covid questions and added integration tests stub

---
+++ de-exceptional-closures-unitTests/Integration/BaseIntegration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace de_exceptional_closures_unitTests.Integration
+{
+    public class BaseIntegration
+    {
+        public BaseIntegration()
+        {
+
+        }
+    }
+}
