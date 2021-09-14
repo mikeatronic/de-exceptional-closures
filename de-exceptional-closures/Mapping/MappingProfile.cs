0c8c84472a48a1cb9ca17d42f6c07e4583ee7566
michael.stevenson <michael.stevenson@finance-ni.gov.uk>
2021-09-14 10:55:09 +0100

    SQ fixes

--- de-exceptional-closures/Mapping/MappingProfile.cs
+++ de-exceptional-closures/Mapping/MappingProfile.cs
@@ -5,6 +5,7 @@ using de_exceptional_closures.ViewModels.Closure;
 using de_exceptional_closures.ViewModels.Home;
 using de_exceptional_closures_core.Common;
 using de_exceptional_closures_core.Dtos;
+using System;
 
 namespace de_exceptional_closures.Mapping
 {
@@ -27,12 +28,6 @@ namespace de_exceptional_closures.Mapping
             .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
 
             CreateMap<IndexViewModel, ClosureReasonDto>()
-                  .ForMember(d => d.CovidQ1, o => o.MapFrom(s => s.ReasonTypeId == (int)OtherReasonType.Covid ? s.CovidQ1 : null))
-                  .ForMember(d => d.CovidQ2, o => o.MapFrom(s => s.ReasonTypeId == (int)OtherReasonType.Covid ? s.CovidQ2 : null))
-                  .ForMember(d => d.CovidQ3, o => o.MapFrom(s => s.ReasonTypeId == (int)OtherReasonType.Covid ? s.CovidQ3 : null))
-                  .ForMember(d => d.CovidQ4, o => o.MapFrom(s => s.ReasonTypeId == (int)OtherReasonType.Covid ? s.CovidQ4 : null))
-                  .ForMember(d => d.CovidQ5, o => o.MapFrom(s => s.ReasonTypeId == (int)OtherReasonType.Covid ? s.CovidQ5 : null))
-                     .ForMember(d => d.OtherReason, o => o.MapFrom(s => s.ReasonTypeId == (int)OtherReasonType.Other ? s.OtherReason : null))
             .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
 
             CreateMap<ClosureReasonDto, IndexViewModel>()
